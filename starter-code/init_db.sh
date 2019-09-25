5eef042f6228f37eca3d77542e9ef0e5ca2eee6c
Melissa Jeremie <melissajeremie@gmail.com>
2019-09-25 17:45:00 -0400

    finished HW 18

--- starter-code/init_db.sh
+++ starter-code/init_db.sh
@@ -2,4 +2,30 @@
 
 # TODO: Create database and initialize database table structure
 
+psql postgres -c 'CREATE DATABASE joinhw;'
+
+psql -d joinhw -c 'BEGIN;
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY NOT NULL,
+    ssn VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    year_of_birth INT NOT NULL,
+    city VARCHAR(255) NOT NULL
+);
+COMMIT;'
+psql -d joinhw -c 'BEGIN;
+CREATE TABLE jobs (
+    id SERIAL PRIMARY KEY NOT NULL,
+    ssn VARCHAR(255) NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    salary INT NOT NULL,
+    experience INT NOT NULL,
+    FOREIGN KEY (ssn) REFERENCES employees(ssn)
+);
+COMMIT;'
+
 # TODO: Insert data into tables
+
+psql -d joinhw -c "COPY employees(ssn,first_name,last_name,year_of_birth,city) FROM '/Users/mkj/Desktop/MYAMEX/Homework/week05/03-Wednesday/joins/starter-code/employees.csv' DELIMITER ',' CSV HEADER";
+psql -d joinhw -c "COPY jobs(ssn,company,salary,experience) FROM '/Users/mkj/Desktop/MYAMEX/Homework/week05/03-Wednesday/joins/starter-code/jobs.csv' DELIMITER ',' CSV HEADER";
--- starter-code/run_queries.sh
+++ starter-code/run_queries.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 # TODO: Write queries to display:
+
 # Employees working at macys
+SELECT first_name, last_name FROM employees INNER JOIN jobs ON employees.ssn = jobs.ssn WHERE company = 'Macy''s';
+
 # Companies in Boston
+SELECT company FROM jobs INNER JOIN employees ON employees.ssn = jobs.ssn WHERE city = 'Boston';
+
 # Employee with the highest salary
+
+SELECT first_name, last_name FROM employees INNER JOIN jobs ON employees.ssn = jobs.ssn ORDER BY salary DESC LIMIT 1;
